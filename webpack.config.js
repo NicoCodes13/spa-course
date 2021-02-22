d6e2fb53bee2b3803ac680371e342426e0a3ea2a
Nico Castañeda <nickcar13@gmail.com>
2021-02-22 13:07:53 -0500

    Añadiendo scripts y test rapido de la pagina

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,32 @@
-const path = require('path'); //usado para movernos en las carpetas del proyecto
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //Para el uso del html desde webpack
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-//modulo de configuracion
 module.exports = {
-  entry: './src/index.js', //objeto de donde va venir la informacion final
-  //configuracion de donde y como se llamara el archivo de salida
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), //carpeta de salida
-    filename: 'main.js', //nombre del archivo de salida
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
   },
-  // extenciones de los archivos a usar e interpretar webpack
   resolve: {
     extensions: ['.js'],
   },
-  // generar las reglas para trabajar
   module: {
     rules: [
-      //para babel
       {
-        test: /\.js?$/, //-> rejex para tomar en cuenta los archivos .js
-        exclude: /node_modules/, //-> excluir los nodemodles de babel
+        test: /\.js?$/,
+        exclude: /node_modules/,
         use: {
-          loader: 'babel-loader', //-> usasndo el loader de babel para esta regla
+          loader: 'babel-loader',
         },
       },
     ],
   },
-  //configurar plugins a usar
   plugins: [
-    //creandolos
-    new HtmlWebpackPlugin([
+    new HtmlWebPackPlugin([
       {
-        inject: true, //->permitir injectar o poner codigo en un archivo
-        template: './public/index.html', //-> archivo que permitira la inyeccion
-        filename: './index.html', //-> archivo de salida del plugin
+        inject: true,
+        template: './public/index.html',
+        filename: './index.html',
       },
     ]),
   ],
