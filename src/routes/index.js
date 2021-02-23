6a937d20ff067ee8a289b1c35e5e6f14d7008c93
Nico Castañeda <nickcar13@gmail.com>
2021-02-23 13:04:46 -0500

    logica de conexion de las rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -19,6 +19,10 @@ const router = async () => {
 
   //generando el header
   header.innerHTML = await Header();
+  let hash = getHash();
+  let route = await resolveRoutes(hash);
+  let render = routes[route] ? routes[route] : Error404;
+  content.innerHTML = await render();
 };
 
 export default router;
